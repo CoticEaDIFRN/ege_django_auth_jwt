dbe8bf24c49d1bf1f6b43cb541168cca947b1a52
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2018-12-26 14:16:40 -0300

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='ege_django_auth_jwt',
     packages=['ege_django_auth_jwt', ],
-    version='1.1',
-    download_url='https://github.com/CoticEaDIFRN/ege_django_auth_jwt/releases/tag/1.1',
+    version='1.0.1',
+    download_url='https://github.com/CoticEaDIFRN/ege_django_auth_jwt/releases/tag/1.0.1',
     description='JWT authentication for Django from EGE project',
     long_description='JWT authentication for Django EGE project',
     author='Kelson da Costa Medeiros',
