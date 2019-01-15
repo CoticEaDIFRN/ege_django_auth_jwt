902f2bb66ff2e05b1ad154c81e45c80af3c0f8e4
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-01-15 18:50:31 -0300

    Release 1.2.0

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(
-    name='ege_auth_jwt',
-    packages=['ege_auth_jwt', ],
-    version='1.1.1',
-    download_url='https://github.com/CoticEaDIFRN/ege_auth_jwt/releases/tag/1.1.1',
-    description='JWT authentication for Django from EGE project',
-    long_description='JWT authentication for Django EGE project',
+    name='ege_utils',
+    packages=['ege_utils', ],
+    version='1.2.0',
+    download_url='https://github.com/CoticEaDIFRN/ege_utils/releases/tag/1.2.0',
+    description='Utils classes for EGE project',
+    long_description='Utils classes for EGE project',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@ifrn.edu.br',
-    url='https://github.com/CoticEaDIFRN/ege_auth_jwt',
+    url='https://github.com/CoticEaDIFRN/ege_utils',
     keywords=['EGE', 'JWT', 'Django', 'Auth', 'SSO', 'client', ],
     install_requires=['PyJWT==1.7.1', 'requests==2.21.0', 'django>=2.0,<3.0'],
     classifiers=[]
